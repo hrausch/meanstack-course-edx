f30b489e3d6f7f1d652f82bb83dc725fcfd5c542
HERBERT RAUSCH <hrausch@gmail.com>
2016-04-12 21:29:07 -0300

    Added files via upload
    Initial files. Homework chapter 1.

---
+++ chapter1/interface.js
@@ -0,0 +1,19 @@
+/*
+ *  Inserts "doc" into the collection "movies".
+ */
+exports.insert = function(db, doc, callback) {
+    
+	db.collection('movies').insert(doc);  
+  callback(null);
+};
+
+/*
+ *  Finds all documents in the "movies" collection
+ *  whose "director" field equals the given director,
+ *  ordered by the movie's "title" field. See
+ *  http://mongodb.github.io/node-mongodb-native/2.0/api/Cursor.html#sort
+ */
+exports.byDirector = function(db, director, callback) {
+         
+  callback(null, db.collection('sample').find().toArray() );
+};
